8ff559418274cdfa0c234c572aa9eac360f9a1d8
customcommander <hello@spinjs.com>
2021-04-25 19:47:22 +0000

    refactor: redesign entry ops map

--- src/parser.js
+++ src/parser.js
@@ -24,87 +24,26 @@ const defaults =
         }
       , acc );
 
-const OPS =
-  { A1: append
-  , A2: append
-  , A3: append
-  , A4: append
-  , AB: add
-  , AD: add
-  , AN: add
-  , AU: append
-  , AV: add
-  , BT: add
-  , C1: add
-  , C2: add
-  , C3: add
-  , C4: add
-  , C5: add
-  , C6: add
-  , C7: add
-  , C8: add
-  , CA: add
-  , CN: add
-  , CP: add
-  , CT: add
-  , CY: add
-  , DA: add
-  , DB: add
-  , DO: add
-  , DP: add
-  , ED: add
-  , EP: add
-  , ET: add
-  , ID: add
-  , IS: add
-  , J1: add
-  , J2: add
-  , JA: add
-  , JF: add
-  , JO: add
-  , KW: append
-  , L1: add
-  , L2: add
-  , L3: add
-  , L4: add
-  , LA: add
-  , LB: add
-  , LK: add
-  , M1: add
-  , M2: add
-  , M3: add
-  , N1: add
-  , N2: add
-  , NV: add
-  , OP: add
-  , PB: add
-  , PP: add
-  , PY: add
-  , RI: add
-  , RN: add
-  , RP: add
-  , SE: add
-  , SN: add
-  , SP: add
-  , ST: add
-  , T1: add
-  , T2: add
-  , T3: add
-  , TA: append
-  , TI: add
-  , TT: add
-  , TY: add
-  , U1: add
-  , U2: add
-  , U3: add
-  , U4: add
-  , U5: add
-  , UR: append
-  , VL: add
-  , VO: add
-  , Y1: add
-  , Y2: add
-  };
+const OPS = { A1: append, A2: append, A3: append, A4: append
+            , AB: add   , AD: add   , AN: add   , AU: append
+            , AV: add   , BT: add   , C1: add   , C2: add
+            , C3: add   , C4: add   , C5: add   , C6: add
+            , C7: add   , C8: add   , CA: add   , CN: add
+            , CP: add   , CT: add   , CY: add   , DA: add
+            , DB: add   , DO: add   , DP: add   , ED: add
+            , EP: add   , ET: add   , ID: add   , IS: add
+            , J1: add   , J2: add   , JA: add   , JF: add
+            , JO: add   , KW: append, L1: add   , L2: add
+            , L3: add   , L4: add   , LA: add   , LB: add
+            , LK: add   , M1: add   , M2: add   , M3: add
+            , N1: add   , N2: add   , NV: add   , OP: add
+            , PB: add   , PP: add   , PY: add   , RI: add
+            , RN: add   , RP: add   , SE: add   , SN: add
+            , SP: add   , ST: add   , T1: add   , T2: add
+            , T3: add   , TA: append, TI: add   , TT: add
+            , TY: add   , U1: add   , U2: add   , U3: add
+            , U4: add   , U5: add   , UR: append, VL: add
+            , VO: add   , Y1: add   , Y2: add };
 
 const process_ast =
   ast =>
