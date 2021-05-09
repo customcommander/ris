a048ebb970e8716164f137ef37bfc3745da86f74
customcommander <hello@spinjs.com>
2021-05-09 09:27:09 +0000

    chore: fix copyright & license annotations in main module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /**
- * Copyright (c) 2021 Julien Gonzalez <hello@spinjs.com>
+ * @copyright (c) 2021 Julien Gonzalez <hello@spinjs.com>
+ * @license MIT
  */
 module.exports = require('./parser.js');
 module.exports.toMendeley = require('./mendeley').to;
